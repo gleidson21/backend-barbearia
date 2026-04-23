934d007f817e831469f1bd554be051ae85271e2d
gledson <gledsonalcantara4@gmail.com>
2026-04-23 14:37:13 -0400

    atualização no server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
-import './database/index.js'
-const Port = 3000;
-app.listen(Port, () => {
-	console.log(`🚀 servidor rodando na port ${Port}`);
+import "./database/index.js";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 servidor rodando na porta ${PORT}`);
 });
