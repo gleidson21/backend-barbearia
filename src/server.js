9a40c1a76626cc6c8551a5f7a5814a9282ffcb1a
gledson <gledsonalcantara4@gmail.com>
2026-04-23 15:03:44 -0400

    serve atualizado

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import "./database/index.js";
 
 const PORT = process.env.PORT || 3000;
 
+process.on("uncaughtException", (error) => {
+  console.error("uncaughtException:", error);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("unhandledRejection:", reason);
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 servidor rodando na porta ${PORT}`);
 });
