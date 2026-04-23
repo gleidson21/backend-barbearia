6f08a9083dce853f5e6a663121da1ea22e7d4858
gledson <gledsonalcantara4@gmail.com>
2026-04-23 15:05:07 -0400

    app atualizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
-import routes from "./routes.js"; // Importa o arquivo que acabamos de criar
-import "./database/index.js";    // Importa a sua conexão com o banco (Database)
-import cors from 'cors'
+import routes from "./routes.js";
+import cors from "cors";
+
 class App {
   constructor() {
     this.app = express();
@@ -12,12 +12,10 @@ class App {
 
   middlewares() {
     this.app.use(cors());
-    // Permite que a API entenda requisições no formato JSON
     this.app.use(express.json());
   }
 
   routes() {
-    // Avisa ao servidor para usar as rotas configuradas
     this.app.use(routes);
   }
 }
